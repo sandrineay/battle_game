5a156297fdeabaa21a81606351f46cd8b054f1e2
Sandrine Ayral <sandrine.ayral@gmail.com>
2019-03-24 19:59:55 -0400

    Add images to seed + Add image style

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
 puts 'Create first 3 players'
 
-Player.create(name: 'Gandalf', strength_points: 2, intelligence_points: 4, magic_points: 4)
-Player.create(name: 'Aragorn', strength_points: 4, intelligence_points: 5, magic_points: 1)
-Player.create(name: 'Uruk Hai', strength_points: 7, intelligence_points: 2, magic_points: 1)
+player = Player.new(name: 'Gandalf', strength_points: 2, intelligence_points: 4, magic_points: 4)
+player.remote_picture_url = 'https://raw.githubusercontent.com/sandrineay/pdv-images/master/gandalf.png'
+player.save
+
+player = Player.create(name: 'Aragorn', strength_points: 4, intelligence_points: 5, magic_points: 1)
+player.remote_picture_url = 'https://raw.githubusercontent.com/sandrineay/pdv-images/master/aragorn.png'
+player.save
+
+player = Player.create(name: 'Uruk Hai', strength_points: 7, intelligence_points: 2, magic_points: 1)
+player.remote_picture_url = 'https://raw.githubusercontent.com/sandrineay/pdv-images/master/uruk-hai.png'
+player.save
 
 puts "Created #{Player.count} players: #{Player.first.name}, #{Player.second.name} and #{Player.third.name} 👌"
