ecf61f7660f60161b05bc7afa957059f8e5c44ee
Sandrine Ayral <sandrine.ayral@gmail.com>
2019-03-25 14:04:11 -0400

    Write basic create method for Battle

--- app/controllers/battles_controller.rb
+++ app/controllers/battles_controller.rb
@@ -6,8 +6,20 @@ class BattlesController < ApplicationController
   end
 
   def create
+    @battle = Battle.new(battle_params)
+    if @battle.save
+      redirect_to battle_path(@battle)
+    else
+      render :new
+    end
   end
 
   def show
+  end
+
+  private
+
+  def battle_params
+    params.require(:battle).permit(:player_1_id, :player_2_id)
   end
 end
