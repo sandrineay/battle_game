08e579169515d1dffecc01d87cc1043764b1e0e8
Sandrine Ayral <sandrine.ayral@gmail.com>
2019-03-26 16:04:22 -0400

    Correct seed file style and add one player

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 puts 'Delete existing data'
 
 Battle.destroy_all
@@ -5,16 +7,44 @@ Player.destroy_all
 
 puts 'Create first 3 players'
 
-player = Player.new(name: 'Gandalf', strength_points: 2, intelligence_points: 4, magic_points: 4)
+player = Player.new(
+  name: 'Gandalf',
+  strength_points: 2,
+  intelligence_points: 4,
+  magic_points: 4
+)
 player.remote_picture_url = 'https://raw.githubusercontent.com/sandrineay/pdv-images/master/gandalf.png'
 player.save
 
-player = Player.create(name: 'Aragorn', strength_points: 4, intelligence_points: 5, magic_points: 1)
+player = Player.create(
+  name: 'Aragorn',
+  strength_points: 4,
+  intelligence_points: 5,
+  magic_points: 1
+)
 player.remote_picture_url = 'https://raw.githubusercontent.com/sandrineay/pdv-images/master/aragorn.png'
 player.save
 
-player = Player.create(name: 'Uruk Hai', strength_points: 7, intelligence_points: 2, magic_points: 1)
+player = Player.create(
+  name: 'Uruk Hai',
+  strength_points: 7,
+  intelligence_points: 2,
+  magic_points: 1
+)
 player.remote_picture_url = 'https://raw.githubusercontent.com/sandrineay/pdv-images/master/uruk-hai.png'
 player.save
 
-puts "Created #{Player.count} players: #{Player.first.name}, #{Player.second.name} and #{Player.third.name} 👌"
+player = Player.new(
+  name: 'Legolas',
+  strength_points: 3,
+  intelligence_points: 4,
+  magic_points: 3
+)
+player.remote_picture_url = 'https://raw.githubusercontent.com/sandrineay/pdv-images/master/legolas.jpg'
+player.save
+
+puts "Created #{Player.count} players:
+  #{Player.first.name},
+  #{Player.second.name},
+  #{Player.third.name} and
+  #{Player.fourth.name} 👌"
