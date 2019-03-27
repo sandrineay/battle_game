63fa2fc8d37db89ebf558ec786a943fb87c83789
Sandrine Ayral <sandrine.ayral@gmail.com>
2019-03-27 19:16:42 -0400

    Add three unit tests for player

--- test/models/player_test.rb
+++ test/models/player_test.rb
@@ -3,15 +3,33 @@
 require 'test_helper'
 
 class PlayerTest < ActiveSupport::TestCase
-  test 'score returns the right potential value' do
-    player = Player.new(
-      name: 'gimli',
-      attack_points: 1.0,
-      life_points: 3.0,
-      intelligence_points: 3,
-      magic_points: 1,
-      strength_points: 6
+  def setup
+    @player = Player.new(
+      name: 'Player Name',
+      strength_points: 5,
+      intelligence_points: 4,
+      magic_points: 6
     )
-    assert(player.score.between?(5, 9))
+  end
+
+  test 'valid player' do
+    assert @player.valid?
+  end
+
+  test 'invalid without name' do
+    @player.name = nil
+    refute @player.valid?, 'player is valid without a name'
+    assert_not_nil @player.errors[:name], 'no validation error for name present'
+  end
+
+  test 'invalid if name is not unique' do
+    Player.create(
+      name: 'Player Name',
+      strength_points: 3,
+      intelligence_points: 6,
+      magic_points: 5
+    )
+    refute @player.valid?, 'player is valid without a unique name'
+    assert_not_nil @player.errors[:name], 'no validation error for name unique'
   end
 end
